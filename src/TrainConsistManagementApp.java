6b9018a4f8f5cf3426bd013ff67bd08fffd5f1e1
khushi9990 <khushi68kk@gmail.com>
2026-03-18 15:39:29 +0530

    UC4:Maintain Ordered Bogie IDs

--- src/TrainConsistManagementApp.java
+++ src/TrainConsistManagementApp.java
@@ -1,29 +1,38 @@
-import java.util.HashSet;
-import java.util.Set;
+import java.util.LinkedList;
 
 public class TrainConsistManagementApp {
 
     public static void main(String[] args) {
 
         System.out.println("================================");
-        System.out.println("UC3 - Track Unique Bogie IDs");
+        System.out.println("UC4 - Maintain Ordered Bogie Consist");
         System.out.println("================================\n");
 
-        Set<String> bogieIDs = new HashSet<>();
+        LinkedList<String> consist = new LinkedList<>();
 
-        bogieIDs.add("BG104");
-        bogieIDs.add("BG103");
-        bogieIDs.add("BG102");
-        bogieIDs.add("BG101");
-        bogieIDs.add("BG101"); // duplicate
-        bogieIDs.add("BG102"); // duplicate
+        // Add bogies
+        consist.add("Engine");
+        consist.add("Sleeper");
+        consist.add("AC");
+        consist.add("Cargo");
+        consist.add("Guard");
 
-        System.out.println("Bogie IDs After Insertion:");
-        System.out.println(bogieIDs + "\n");
+        System.out.println("Initial Train Consist:");
+        System.out.println(consist + "\n");
 
-        System.out.println("Note:");
-        System.out.println("Duplicates are automatically ignored by HashSet.\n");
+        // Insert Pantry Car at position 2 (0-based index)
+        consist.add(2, "Pantry Car");
 
-        System.out.println("UC3 uniqueness validation completed...");
+        System.out.println("After Inserting 'Pantry Car' at position 2:");
+        System.out.println(consist + "\n");
+
+        // Remove first and last bogie
+        consist.removeFirst();
+        consist.removeLast();
+
+        System.out.println("After Removing First and Last Bogie:");
+        System.out.println(consist + "\n");
+
+        System.out.println("UC4 ordered consist operations completed...");
     }
 }
